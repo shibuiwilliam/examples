5a282f357b0becacf95fcac2bae61074072e284a
shibuiwilliam <shibuiyusuke@gmail.com>
2020-04-29 15:03:53 +0900

    update

---
+++ lite/examples/model_personalization/android/app/src/main/java/org/tensorflow/lite/examples/transfer/ImageUtils.kt
@@ -0,0 +1,96 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+package org.tensorflow.lite.examples.transfer
+
+/** Utility class for manipulating images.
+ *
+ * Copy-pasted from TFLite object detection example.
+ */
+object ImageUtils {
+    // TODO(b/137790961) reduce code duplication between different TFLite examples.
+    // This value is 2 ^ 18 - 1, and is used to clamp the RGB values before their ranges
+    // are normalized to eight bits.
+    const val MAX_CHANNEL_VALUE = 262143
+    fun convertYUV420SPToARGB8888(input: ByteArray, width: Int, height: Int, output: IntArray) {
+        val frameSize = width * height
+        var j = 0
+        var yp = 0
+        while (j < height) {
+            var uvp = frameSize + (j shr 1) * width
+            var u = 0
+            var v = 0
+            var i = 0
+            while (i < width) {
+                val y = 0xff and input[yp].toInt()
+                if (i and 1 == 0) {
+                    v = 0xff and input[uvp++].toInt()
+                    u = 0xff and input[uvp++].toInt()
+                }
+                output[yp] = yuv2Rgb(y, u, v)
+                i++
+                yp++
+            }
+            j++
+        }
+    }
+
+    private fun yuv2Rgb(y: Int, u: Int, v: Int): Int {
+        // Adjust and check YUV values
+        var y = y
+        var u = u
+        var v = v
+        y = if (y - 16 < 0) 0 else y - 16
+        u -= 128
+        v -= 128
+
+        // This is the floating point equivalent. We do the conversion in integer
+        // because some Android devices do not have floating point in hardware.
+        // nR = (int)(1.164 * nY + 2.018 * nU);
+        // nG = (int)(1.164 * nY - 0.813 * nV - 0.391 * nU);
+        // nB = (int)(1.164 * nY + 1.596 * nV);
+        val y1192 = 1192 * y
+        var r = y1192 + 1634 * v
+        var g = y1192 - 833 * v - 400 * u
+        var b = y1192 + 2066 * u
+
+        // Clipping RGB values to be inside boundaries [ 0 , MAX_CHANNEL_VALUE ]
+        r = if (r > MAX_CHANNEL_VALUE) MAX_CHANNEL_VALUE else Math.max(r, 0)
+        g = if (g > MAX_CHANNEL_VALUE) MAX_CHANNEL_VALUE else Math.max(g, 0)
+        b = if (b > MAX_CHANNEL_VALUE) MAX_CHANNEL_VALUE else Math.max(b, 0)
+        return -0x1000000 or (r shl 6 and 0xff0000) or (g shr 2 and 0xff00) or (b shr 10 and 0xff)
+    }
+
+    @JvmStatic
+    fun convertYUV420ToARGB8888(
+            yData: ByteArray,
+            uData: ByteArray,
+            vData: ByteArray,
+            width: Int,
+            height: Int,
+            yRowStride: Int,
+            uvRowStride: Int,
+            uvPixelStride: Int,
+            out: IntArray) {
+        var yp = 0
+        for (j in 0 until height) {
+            val pY = yRowStride * j
+            val pUV = uvRowStride * (j shr 1)
+            for (i in 0 until width) {
+                val uvOffset = pUV + (i shr 1) * uvPixelStride
+                out[yp++] = yuv2Rgb(0xff and yData[pY + i].toInt(), 0xff and uData[uvOffset].toInt(), 0xff and vData[uvOffset].toInt())
+            }
+        }
+    }
+}
